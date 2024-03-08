444a5b28ba8319385dd43f6ea3fe31d8a6ef4082
cris <carincon93@gmail.com>
2024-03-08 16:40:24 -0500

    Planet animated

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,12 +2,21 @@
 
 import React, { useRef, useEffect } from 'react'
 import * as THREE from 'three'
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js'
 import './styles.css'
 // import { Logo } from '../components/Logo'
 
 interface Game {
     speed: number
+
     planetRadius: number
+    planetLength: number
+
+    wavesMinAmp: number
+    wavesMaxAmp: number
+    wavesMinSpeed: number
+    wavesMaxSpeed: number
+
     status: string
 }
 
@@ -21,10 +30,12 @@ const GameScene: React.FC = () => {
     let shadowLight: THREE.DirectionalLight
     let planet: Planet
     let space: Space
+    let asteroid: Asteroid
+    let rocket: Rocket
     let game: Game
     let deltaTime: number = 0
-    // let newTime: number = new Date().getTime()
-    // let oldTime: number = new Date().getTime()
+    let newTime: number = new Date().getTime()
+    let oldTime: number = new Date().getTime()
 
     const Colors = {
         red: 0xf25346,
@@ -39,7 +50,15 @@ const GameScene: React.FC = () => {
     function resetGame() {
         game = {
             speed: 0,
+
             planetRadius: 600,
+            planetLength: 800,
+
+            wavesMinAmp: 5,
+            wavesMaxAmp: 20,
+            wavesMinSpeed: 0.001,
+            wavesMaxSpeed: 0.003,
+
             status: 'playing',
         }
     }
@@ -55,6 +74,7 @@ const GameScene: React.FC = () => {
         const fieldOfView = 50
         const nearPlane = 0.1
         const farPlane = 10000
+
         camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane)
         camera.position.x = 0
         camera.position.z = 200
@@ -68,7 +88,12 @@ const GameScene: React.FC = () => {
     }
 
     function loop() {
+        newTime = new Date().getTime()
+        deltaTime = newTime - oldTime
+        oldTime = newTime
+
         space.moveStars()
+        planet.moveWaves()
 
         renderer.render(scene, camera)
 
@@ -94,21 +119,62 @@ const GameScene: React.FC = () => {
     }
 
     class Planet {
+        private waves: { y: number; x: number; z: number; ang: number; amp: number; speed: number }[]
         mesh: THREE.Mesh
 
         constructor() {
-            const geom = new THREE.CylinderGeometry(600, 100, 1000, 100, 10)
+            const geom = new THREE.CylinderGeometry(150, 150, 10, 10, 10)
+            const mergedGeometry = BufferGeometryUtils.mergeVertices(geom)
             geom.applyMatrix4(new THREE.Matrix4().makeRotationX(-Math.PI / 2))
 
+            const positionAttribute = mergedGeometry.getAttribute('position')
+            const l = positionAttribute.count
+
+            this.waves = []
+
+            for (let i = 0; i < l; i++) {
+                const x = positionAttribute.getX(i)
+                const y = positionAttribute.getY(i)
+                const z = positionAttribute.getZ(i)
+
+                this.waves.push({
+                    y: y,
+                    x: x,
+                    z: z,
+                    ang: Math.random() * Math.PI * 2,
+                    amp: game.wavesMinAmp + Math.random() * (game.wavesMaxAmp - game.wavesMinAmp),
+                    speed: game.wavesMinSpeed + Math.random() * (game.wavesMaxSpeed - game.wavesMinSpeed),
+                })
+            }
+
             const mat = new THREE.MeshPhongMaterial({
-                color: 0x68c3c0,
+                color: 0xe4505b,
                 transparent: true,
-                opacity: 0.6,
-                // shading: THREE.FlatShading,
+                opacity: 0.8,
+                shading: THREE.FlatShading,
             })
 
-            this.mesh = new THREE.Mesh(geom, mat)
+            this.mesh = new THREE.Mesh(mergedGeometry, mat)
+            this.mesh.name = 'waves'
             this.mesh.receiveShadow = true
+        }
+
+        moveWaves() {
+            const positionAttribute = this.mesh.geometry.getAttribute('position')
+            const l = positionAttribute.count
+            for (let i = 0; i < l; i++) {
+                let x = positionAttribute.getX(i)
+                let y = positionAttribute.getY(i)
+                let z = positionAttribute.getZ(i)
+
+                const vprops = this.waves[i]
+                x = vprops.x + Math.cos(vprops.ang) * vprops.amp
+                y = vprops.y + Math.sin(vprops.ang) * vprops.amp
+                vprops.ang += 0.005
+
+                positionAttribute.setXYZ(i, x, y, z)
+            }
+            this.mesh.geometry.attributes.position.needsUpdate = true
         }
     }
 
@@ -185,9 +251,82 @@ const GameScene: React.FC = () => {
         }
     }
 
+    class Asteroid {
+        mesh: THREE.Group
+        material: THREE.MeshStandardMaterial
+
+        constructor() {
+            this.material = new THREE.MeshStandardMaterial({ color: 0xcccccc })
+
+            this.mesh = new THREE.Group()
+            this.createAsteroid()
+            // this.addLights()
+
+            // this.animate()
+        }
+
+        private createAsteroid() {
+            const numFragments = 6
+
+            for (let i = 0; i < numFragments; i++) {
+                const fragment = this.createFragment()
+                fragment.position.set((Math.random() - 0.5) * 3, (Math.random() - 0.5) * 3, (Math.random() - 0.5) * 3)
+                fragment.rotation.set(Math.random() * Math.PI * 2, Math.random() * Math.PI * 2, Math.random() * Math.PI * 2)
+
+                this.mesh.add(fragment)
+            }
+        }
+
+        private createFragment(): THREE.Mesh {
+            const geometry = new THREE.BoxGeometry(0.5 + Math.random(), 0.5 + Math.random(), 0.5 + Math.random())
+            const fragment = new THREE.Mesh(geometry, this.material)
+            return fragment
+        }
+
+        animate() {
+            this.mesh.rotation.x += 0.01
+            this.mesh.rotation.y += 0.01
+        }
+    }
+
+    class Rocket {
+        mesh: THREE.Group
+        material: THREE.MeshStandardMaterial
+
+        constructor() {
+            this.material = new THREE.MeshStandardMaterial({ color: 0xcccccc })
+
+            this.mesh = new THREE.Group()
+
+            this.createRocket()
+        }
+
+        private createRocket() {
+            const bodyGeometry = new THREE.CylinderGeometry(0.5, 0.5, 2, 6)
+            const body = new THREE.Mesh(bodyGeometry, this.material)
+            this.mesh.add(body)
+
+            const coneGeometry = new THREE.CylinderGeometry(0.2, 0, 0.5, 6)
+            const cone = new THREE.Mesh(coneGeometry, this.material)
+            cone.position.y = 1.5
+            this.mesh.add(cone)
+
+            const wingGeometry = new THREE.BoxGeometry(1, 0.1, 0.1)
+            const wing1 = new THREE.Mesh(wingGeometry, this.material)
+            wing1.position.set(0.5, 0.8, 0)
+            this.mesh.add(wing1)
+
+            const wing2 = new THREE.Mesh(wingGeometry, this.material)
+            wing2.position.set(-0.5, 0.8, 0)
+            this.mesh.add(wing2)
+
+            this.mesh.scale.set(4, 4, 4)
+        }
+    }
+
     function createPlanet() {
         planet = new Planet()
-        planet.mesh.position.y = -game.planetRadius
+        planet.mesh.position.y = 20
         scene.add(planet.mesh)
     }
 
@@ -195,6 +334,19 @@ const GameScene: React.FC = () => {
         space = new Space()
         space.mesh.position.y = -game.planetRadius
         scene.add(space.mesh)
+    }
+
+    function createAsteroid() {
+        asteroid = new Asteroid()
+        asteroid.mesh.position.y = 130
+        asteroid.mesh.scale.set(4, 4, 4)
+        scene.add(asteroid.mesh)
+    }
+
+    function createRocket() {
+        rocket = new Rocket()
+        rocket.mesh.position.y = 100
+        scene.add(rocket.mesh)
     }
 
     useEffect(() => {
@@ -205,6 +357,8 @@ const GameScene: React.FC = () => {
             createLights()
             createSpace()
             createPlanet()
+            createAsteroid()
+            // createRocket()
             loop()
         }
     }, [])
