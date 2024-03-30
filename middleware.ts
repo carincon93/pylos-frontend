e50fa9f7732a85e8234b2b291eb59d68f2dfe21b
Cristian <carincon93@gmail.com>
2024-03-29 20:19:34 -0500

    Middleware logic modified

--- middleware.ts
+++ middleware.ts
@@ -2,23 +2,37 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getTokenData } from './utils/getTokenData'
 import { isTokenExpired } from './utils/isTokenExpired'
+import { EMPEZAR_AVENTURA_ROUTE, REGISTER_ROUTE, HOME_ROUTE, LOGIN_ROUTE, SUBFOLDER_ROUTE } from '@/utils/routes'
 
 export function middleware(request: NextRequest) {
     const accessToken = request.cookies.get('accessToken')?.value
 
-    // const secretKey = process.env.NEXT_PUBLIC_NESTJS_JWT_SECRET || '' // Replace with your actual secret key
+    const secretKey = process.env.NEXT_PUBLIC_NESTJS_JWT_SECRET || '' // Replace with your actual secret key
 
-    // if (accessToken) {
-    //     if (isTokenExpired(accessToken, secretKey)) {
-    //         // Access token has expired, remove the token cookie
-    //         request.cookies.delete('accessToken')
-    //     }
-    // }
+    if (accessToken) {
+        if (isTokenExpired(accessToken, secretKey)) {
+            // Access token has expired, remove the token cookie
+            request.cookies.delete('accessToken')
+        }
+    }
+
+    // Check if the path starts with /juego
+    const isJuegoPath = request.nextUrl.pathname.startsWith(SUBFOLDER_ROUTE)
+
+    if (!accessToken && isJuegoPath) {
+        // Redirect to login page if no access token exists for paths under /juego
+        return NextResponse.redirect(new URL(LOGIN_ROUTE, request.url))
+    }
 
     if (!accessToken) {
-        // Redirect to login page if no access token exists
-        if (!request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/registro') && !request.nextUrl.pathname.startsWith('/empezar-aventura')) {
-            return NextResponse.redirect(new URL('/login', request.url))
+        // Redirect to login page if no access token exists for other paths
+        if (
+            !request.nextUrl.pathname.startsWith(HOME_ROUTE) &&
+            !request.nextUrl.pathname.startsWith(LOGIN_ROUTE) &&
+            !request.nextUrl.pathname.startsWith(REGISTER_ROUTE) &&
+            !request.nextUrl.pathname.startsWith(EMPEZAR_AVENTURA_ROUTE)
+        ) {
+            return NextResponse.redirect(new URL(LOGIN_ROUTE, request.url))
         }
         return
     }
@@ -28,12 +42,7 @@ export function middleware(request: NextRequest) {
     if (!tokenData) {
         // Redirect to login page if access token is invalid
         console.error('Token invalid or expired')
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
-
-    // Redirect to /introduccion if access token exists and user is not already on /introduccion page
-    if (!request.nextUrl.pathname.startsWith('/introduccion')) {
-        return NextResponse.redirect(new URL('/introduccion', request.url))
+        return NextResponse.redirect(new URL(LOGIN_ROUTE, request.url))
     }
 }
 
