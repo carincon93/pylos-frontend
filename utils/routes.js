11398ae966ae3fcb49021c559f80472f4e115ad5
Cristian <carincon93@gmail.com>
2024-03-29 20:17:14 -0500

    Routes added

---
+++ utils/routes.js
@@ -0,0 +1,8 @@
+export const HOME_ROUTE = '/'
+export const SUBFOLDER_ROUTE = '/juego'
+export const LOGIN_ROUTE = '/iniciar-sesion'
+export const REGISTER_ROUTE = '/registro'
+export const EMPEZAR_AVENTURA_ROUTE = '/empezar-la-aventura'
+export const INTRODUCCION_ROUTE = `${SUBFOLDER_ROUTE}/introduccion`
+export const MAPA_ROUTE = `${SUBFOLDER_ROUTE}/mapa`
+export const GAME_3D_ROUTE = `${SUBFOLDER_ROUTE}/3d`
