bbab3c6ac4ecbe813edfbe08fd7eef492e7df70d
Cristian <carincon93@gmail.com>
2024-04-03 16:44:30 -0500

    Title Empezar la aventura modified

--- app/(guest)/empezar-la-aventura/page.tsx
+++ app/(guest)/empezar-la-aventura/page.tsx
@@ -4,9 +4,7 @@ import { LOGIN_ROUTE, REGISTER_ROUTE } from '@/utils/routes'
 export default function EmpezarAventuraPage() {
     return (
         <>
-            <div>
-                <h1 className="text-6xl sm:text-8xl text-center font-bold uppercase text-white">Empezar la aventura</h1>
-            </div>
+            <h1 className="text-6xl xl:text-6xl 2xl:text-8xl text-center font-bold uppercase text-white">Empezar la aventura</h1>
 
             <div className="flex flex-col gap-y-4 items-center justify-center lg:justify-start">
                 <Link
