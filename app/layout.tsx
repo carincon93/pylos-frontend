5efc65061d74cb13220cd0a08db8fda9852249d6
Cristian <carincon93@gmail.com>
2024-04-03 16:43:55 -0500

    bg-background added

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <html
             lang="es"
             suppressHydrationWarning>
-            <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)}>
+            <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
                 <AppProvider>
                     <ThemeProvider
                         attribute="class"
