224dc76bbe21b75eae6da7490c8701844b72c25c
Cristian <carincon93@gmail.com>
2024-04-12 20:33:17 -0500

    support ttf json added

--- middleware.ts
+++ middleware.ts
@@ -52,5 +52,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.gltf$|.*\\.glb$|.*\\.webp$|.*\\.mp3|.*\\.(?:png|ico|jpg|jpeg|gif|svg)$).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.gltf$|.*\\.glb$|.*\\.webp$|.*\\.mp3|.*\\.ttf|.*\\.json|.*\\.(?:png|ico|jpg|jpeg|gif|svg)$).*)'],
 }
