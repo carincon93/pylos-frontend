c5576935d347ad868a0e49ec45d2d12de2f21e27
Cristian <carincon93@gmail.com>
2024-04-14 19:28:04 -0500

    Stage added

---
+++ app/components/Stage.jsx
@@ -0,0 +1,25 @@
+import { useGameStore } from '@/lib/store'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react'
+
+export default function Stage(props) {
+    const group = useRef()
+    const { nodes, materials, animations } = useGLTF('/models/Stage.glb')
+
+    return (
+        <group
+            ref={group}
+            {...props}
+            dispose={null}
+            scale={40}>
+            <mesh
+                name="Plane_1"
+                geometry={nodes.Plane_1.geometry}
+                material={materials['Grass']}
+                // material={materials['Mountain']}
+            />
+        </group>
+    )
+}
+
+useGLTF.preload('/models/Stage.glb')
