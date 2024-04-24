993afd98f4f1bbe342ecfc8c4fc18940b7522b3d
Cristian <carincon93@gmail.com>
2024-04-24 11:59:23 -0500

    props added

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,12 @@
 import { Loading } from '@/components/ui/loading'
 
-export default function LoadingOverlay() {
+interface Props {
+    className?: string
+}
+
+export default function LoadingOverlay({ className }: Props) {
     return (
-        <div className="absolute bg-pylos-800 w-full h-[100vh] z-[99] text-white flex items-center justify-center text-4xl font-medium">
+        <div className={`${className} absolute left-0 top-0 right-0 w-full h-[100vh] z-[99] text-white flex items-center justify-center text-4xl font-medium`}>
             <Loading />
         </div>
     )
