fc0d9a7a7f8b4ff9feb491b70e019aa784ce3ea9
Cristian <carincon93@gmail.com>
2024-04-24 13:31:29 -0500

    Fixed error when submit response

--- app/juego/prueba-diagnostica/_prueba.tsx
+++ app/juego/prueba-diagnostica/_prueba.tsx
@@ -84,52 +84,56 @@ export default function Prueba() {
                 <span
                     className={`bg-[url('/estados.png')] size-24 ${
                         opcionCorrecta ? 'bg-[-205px_-9px]' : 'bg-[1px_-21px]'
-                    }  bg-[length:314px] absolute inline-block bg-no-repeat animate__animated animate__bounceIn mx-auto left-0 right-0 top-32 z-30`}></span>
+                    }  bg-[length:314px] absolute inline-block bg-no-repeat animate__animated animate__bounceIn mx-auto left-0 right-0 top-[40%] z-30`}></span>
             )}
 
             <Carousel className="w-[90%] mx-auto h-full flex items-center justify-center -mt-20">
                 <CarouselContent>
-                    {preguntasPruebaDiagnosticaPorUsuario?.map((preguntaPruebaDiagnostica, i) => (
-                        <CarouselItem
-                            key={preguntaPruebaDiagnostica.id}
-                            className="px-10 ">
-                            <p className="text-center text-2xl sm:text-4xl">{preguntaPruebaDiagnostica.pregunta}</p>
+                    {isSubmitting ? (
+                        <LoadingOverlay />
+                    ) : (
+                        preguntasPruebaDiagnosticaPorUsuario?.map((preguntaPruebaDiagnostica, i) => (
+                            <CarouselItem
+                                key={preguntaPruebaDiagnostica.id}
+                                className="px-10 ">
+                                <p className="text-center text-2xl sm:text-4xl">{preguntaPruebaDiagnostica.pregunta}</p>
 
-                            <div className="grid sm:grid-cols-3 gap-4 mt-20">
-                                {preguntaPruebaDiagnostica.esPreguntaCerrada ? (
-                                    <>
-                                        {preguntaPruebaDiagnostica?.opcionPruebaDiagnostica?.map((opcionPruebaDiagnostica, j) => (
+                                <div className="grid sm:grid-cols-3 gap-4 mt-20">
+                                    {preguntaPruebaDiagnostica.esPreguntaCerrada ? (
+                                        <>
+                                            {preguntaPruebaDiagnostica?.opcionPruebaDiagnostica?.map((opcionPruebaDiagnostica, j) => (
+                                                <Button
+                                                    key={opcionPruebaDiagnostica.id}
+                                                    className="p-8 sm:p-10 text-[20px] text-wrap leading-5"
+                                                    onClick={() => {
+                                                        playAudio(0, 2, buttonPressed), handleSubmit(preguntaPruebaDiagnostica.id, opcionPruebaDiagnostica.id, opcionPruebaDiagnostica.esOpcionCorrecta)
+                                                    }}
+                                                    disabled={isSubmitting}>
+                                                    {opcionPruebaDiagnostica.opcion}
+                                                </Button>
+                                            ))}
+                                        </>
+                                    ) : (
+                                        <div className="col-span-3 flex flex-col gap-y-8 items-center justify-center">
+                                            <Input
+                                                className="sm:w-9/12 h-14 border-primary border-2 uppercase text-[18px] text-center mx-auto"
+                                                placeholder="Escriba la respuesta"
+                                                onChange={(event) => setRespuesta(event.target.value)}
+                                            />
                                             <Button
-                                                key={opcionPruebaDiagnostica.id}
-                                                className="p-8 sm:p-10 text-[20px] text-wrap leading-5"
+                                                className="uppercase text-[18px] h-12 w-52"
                                                 onClick={() => {
-                                                    playAudio(0, 2, buttonPressed), handleSubmit(preguntaPruebaDiagnostica.id, opcionPruebaDiagnostica.id, opcionPruebaDiagnostica.esOpcionCorrecta)
+                                                    playAudio(0, 2, buttonPressed), handleSubmit(preguntaPruebaDiagnostica.id, null)
                                                 }}
-                                                disabled={isSubmitting}>
-                                                {opcionPruebaDiagnostica.opcion}
+                                                disabled={respuesta == ''}>
+                                                Siguiente
                                             </Button>
-                                        ))}
-                                    </>
-                                ) : (
-                                    <div className="col-span-3 flex flex-col gap-y-8 items-center justify-center">
-                                        <Input
-                                            className="sm:w-9/12 h-14 border-primary border-2 uppercase text-[18px] text-center mx-auto"
-                                            placeholder="Escriba la respuesta"
-                                            onChange={(event) => setRespuesta(event.target.value)}
-                                        />
-                                        <Button
-                                            className="uppercase text-[18px] h-12 w-52"
-                                            onClick={() => {
-                                                playAudio(0, 2, buttonPressed), handleSubmit(preguntaPruebaDiagnostica.id, null)
-                                            }}
-                                            disabled={respuesta == ''}>
-                                            Siguiente
-                                        </Button>
-                                    </div>
-                                )}
-                            </div>
-                        </CarouselItem>
-                    ))}
+                                        </div>
+                                    )}
+                                </div>
+                            </CarouselItem>
+                        ))
+                    )}
                 </CarouselContent>
             </Carousel>
         </div>
