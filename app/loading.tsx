b52f5cb30444b8935c96c7ce745324ba39876862
Cristian <carincon93@gmail.com>
2024-04-24 13:15:39 -0500

    z-[9999] added

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function LoadingOverlay({ className }: Props) {
     return (
-        <div className={`${className} absolute left-0 top-0 right-0 w-full h-[100vh] z-[99] text-white flex items-center justify-center text-4xl font-medium`}>
+        <div className={`${className} absolute left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex items-center justify-center text-4xl font-medium`}>
             <Loading />
         </div>
     )
