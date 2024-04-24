8991af2461d55fd15e9461d040c2fdd0115ac9ce
Cristian <carincon93@gmail.com>
2024-04-24 09:35:49 -0500

    Background Stars added

---
+++ app/components/BackgroundStars.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Stars } from '@/app/components/Stars'
+import { Canvas } from '@react-three/fiber'
+
+export default function BackgroundStars() {
+    return (
+        <Canvas
+            camera={{ position: [0, 0, 1] }}
+            className="!h-[100vh] !fixed z-10 bg-pylos-800/50">
+            <Stars />
+        </Canvas>
+    )
+}
