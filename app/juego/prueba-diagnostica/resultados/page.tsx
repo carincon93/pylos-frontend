cccee0775e2b248936d89dc171389f5fc4371dc7
Cristian <carincon93@gmail.com>
2024-04-24 12:22:25 -0500

    change button bg color

--- app/juego/prueba-diagnostica/resultados/page.tsx
+++ app/juego/prueba-diagnostica/resultados/page.tsx
@@ -28,7 +28,7 @@ const ResultadosPruebaDiagnosticaPage = ({ searchParams }: Props) => {
 
             <Link
                 href={INTRODUCCION_ROUTE}
-                className="fixed bottom-4 right-4 z-30 bg-sky-600 hover:bg-sky-700 rounded-full shadow-lg py-4 pl-8 pr-6 flex items-center justify-center">
+                className="fixed bottom-4 right-4 z-30 bg-secondary hover:bg-secondary/90 rounded-full shadow-lg py-4 pl-8 pr-6 flex items-center justify-center">
                 Continuar
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
