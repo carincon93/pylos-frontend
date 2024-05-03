d4a088fe6489bf0b98c6b6f105a424bbe8c8f0d0
Cristian <carincon93@gmail.com>
2024-05-02 19:51:11 -0500

    handleRestartPrueba  added

--- app/juego/prueba-diagnostica/resultados/_tabla-posiciones.tsx
+++ app/juego/prueba-diagnostica/resultados/_tabla-posiciones.tsx
@@ -1,16 +1,33 @@
 'use client'
 
 import { fetcher } from '@/utils/fetcher'
-import useSWR from 'swr'
+import useSWR, { mutate } from 'swr'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import LoadingOverlay from '@/app/loading'
+import { Button } from '@/components/ui/button'
+import { restartRespuestaPruebaDiagnostica } from '@/lib/actions'
+import { Usuario } from '@/types/MyTypes'
 
-export default function TablaPosiciones() {
+export default function TablaPosiciones({ isAdmin }: { isAdmin: boolean | undefined }) {
     const { data: resultadosPruebaDiagnostica } = useSWR<[]>(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/respuesta-prueba-diagnostica/obtener/tabla-de-posiciones`, fetcher)
 
     if (!resultadosPruebaDiagnostica) {
         return <LoadingOverlay />
+    }
+
+    const handleRestartPrueba = async (usuarioId: string) => {
+        const data: Partial<Usuario> = {
+            id: usuarioId,
+        }
+
+        try {
+            await restartRespuestaPruebaDiagnostica(data)
+        } catch (error) {
+            console.error('Error al guardar la respuesta:', error)
+        } finally {
+            mutate(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/respuesta-prueba-diagnostica/obtener/tabla-de-posiciones`)
+        }
     }
 
     return (
@@ -21,6 +38,7 @@ export default function TablaPosiciones() {
                     <TableHead className="w-[100px] text-center">Posición</TableHead>
                     <TableHead>Estudiante</TableHead>
                     <TableHead>Puntaje</TableHead>
+                    <TableHead>Tiempo</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
@@ -35,6 +53,14 @@ export default function TablaPosiciones() {
                                 </Avatar>
                                 {resultado.nombre}
                             </div>
+
+                            {isAdmin && (
+                                <Button
+                                    className="text-xs mt-1 bg-red-400 hover:bg-red-500"
+                                    onClick={() => handleRestartPrueba(resultado.usuarioId)}>
+                                    Restablecer prueba
+                                </Button>
+                            )}
                         </TableCell>
                         <TableCell className="font-medium">
                             <div className={`flex items-center bg-pylos-400 text-white rounded-full p-2 justify-center`}>
@@ -46,6 +72,7 @@ export default function TablaPosiciones() {
                                     }  bg-[length:123px] ml-6`}></span>
                             </div>
                         </TableCell>
+                        <TableCell className="font-medium">{resultado.tiempoPruebaDiagnostica} segundos</TableCell>
                     </TableRow>
                 ))}
             </TableBody>
