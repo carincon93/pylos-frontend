cd5e5d3f95bf4b47092c2d3573f301583f865a33
Cristian <carincon93@gmail.com>
2024-05-02 22:10:08 -0500

    checkHealth added

--- app/components/navigation-events.tsx
+++ app/components/navigation-events.tsx
@@ -1,40 +1,67 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { useHealthChecker } from '@/hooks/useHealthChecker'
 import { getProfile } from '@/lib/actions'
 import { useContextData } from '../context/AppContext'
+import LoadingOverlay from '../loading'
 
 export function NavigationEvents() {
     const pathname = usePathname()
     const searchParams = useSearchParams()
+    const [checkingServer, setCheckingServer] = useState(false)
+    const [loading, setLoading] = useState(false)
     const { setProfileUserData } = useContextData()
-
-    const { checking } = useHealthChecker()
 
     useEffect(() => {
         const url = `${pathname}?${searchParams}`
-        console.log(url)
-        // You can now use the current URL
-        // ...
+
+        checkHealth()
     }, [pathname, searchParams])
 
     useEffect(() => {
-        const fetchUserProfile = async () => {
-            try {
-                const profile = await getProfile()
-                setProfileUserData(profile)
-            } catch (error) {
-                console.error('Error fetching data:', error)
-            }
-        }
-
         fetchUserProfile()
     }, [])
 
-    if (checking) {
-        return <div className="fixed top-0 left-0 z-[9999] p-4">El servidor no está respondiendo. Reintentando...</div>
+    const fetchUserProfile = async () => {
+        try {
+            const profile = await getProfile()
+            setProfileUserData(profile)
+        } catch (error) {
+            console.error('Error fetching data:', error)
+        }
+    }
+
+    const checkHealth = async () => {
+        try {
+            setLoading(true)
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/healthcheck`)
+            if (!response.ok) {
+                setLoading(true)
+                setCheckingServer(true) // Cambiamos checking a true si hay un problema
+            } else {
+                setLoading(false)
+                setCheckingServer(false) // Cambiamos checking a false si todo está bien
+            }
+        } catch (error) {
+            console.error('Error checking health:', error)
+            setLoading(true)
+            setCheckingServer(true) // Cambiamos checking a true en caso de error
+        }
+    }
+
+    if (loading && checkingServer) {
+        return (
+            <>
+                <LoadingOverlay className="bg-pylos-800" />
+                <div className="fixed top-0 left-0 z-[10000] p-4">El servidor no está respondiendo. Reintentando...</div>
+            </>
+        )
+    }
+
+    if (loading && !checkingServer) {
+        return <LoadingOverlay className="bg-pylos-800" />
     }
 
     return
