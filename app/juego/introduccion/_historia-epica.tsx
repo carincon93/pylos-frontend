0e37400220285878db73fd874d104c2880d80424
Cristian <carincon93@gmail.com>
2024-05-20 14:45:27 -0500

    Video introduccion added + controls modified

--- app/juego/introduccion/_historia-epica.tsx
+++ app/juego/introduccion/_historia-epica.tsx
@@ -10,7 +10,7 @@ import { updateUsuario } from '@/lib/actions'
 import { MUNDOS_ROUTE } from '@/utils/routes'
 import { Usuario } from '@/types/MyTypes'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { useContextData } from '@/app/context/AppContext'
 
@@ -36,52 +36,39 @@ export default function HistoriaEpica() {
             title: 'First Photo',
             img: '/_e351e41b-efe7-4e59-8ce8-91fbbbd0db45.jpeg',
             date: '4 de abril de 2250',
-            gif: '/parte1.gif',
-            text: 'En la actualidad, los viajes espaciales permiten a las personas explorar planetas lejanos. Imagina a un valiente astronauta al que llamaremos Pylonauta, quien está muy emocionado por descubrir un nuevo mundo llamado Ánfora. <br /> Él parte en su nave espacial, <strong>"NEBULÓN"</strong>, listo para la aventura.',
         },
         {
             id: 'second',
             title: 'Second Photo',
             img: '/_08026eb5-f13f-44f9-8a54-96e796383a0e.jpeg',
-            date: '5 de abril de 2250',
-            gif: '/parte2.gif',
-            text: 'El viaje de Pylonauta está lleno de emoción, pero justo cuando se acerca a Ánfora, una tormenta espacial inesperada aparece. La nave es sacudida violentamente y Pylonauta pierde el control.',
+            date: '4 de abril de 2250',
         },
         {
             id: 'third',
             title: 'Third Photo',
             img: '/_56f5d894-b46b-4294-81d6-17e64e549f11.jpeg',
-            date: '5 de abril de 2250',
-            gif: '/parte3.gif',
-            text: 'Con un estruendo ensordecedor, "NEBULÓN" se estrella contra la superficie del planeta desconocido. Afortunadamente, Pylonauta sale ileso, pero su nave está seriamente dañada y él se encuentra varado en un mundo extraño.',
+            date: '4 de abril de 2250',
         },
         {
             id: 'fourth',
             title: 'Fourth Photo',
             img: '/_2cb2a853-5270-4b83-a92d-9c804ad40b48.jpeg',
-            date: '5 de abril de 2250',
-            gif: '/parte4.gif',
-            text: 'Decidido a encontrar una solución, Pylonauta comienza a explorar Ánfora y pronto descubre que no está solo. Encuentra unas misteriosas pistas que indican que puede reparar su nave, pero primero debe superar una prueba en el antiguo Templo Alameda del Conocimiento.',
+            date: '4 de abril de 2250',
         },
         {
             id: 'fifth',
             title: 'Fifth Photo',
             img: '/_7ff3ae87-8440-4e2e-9599-8262eb0edf5b.jpeg',
-            date: '5 de abril de 2250',
-            gif: '/parte5.gif',
-            text: 'Este templo legendario guarda las herramientas necesarias para reparar la nave, pero solo los más valientes y astutos podrán obtenerlas. El Templo Alameda del Conocimiento está lleno de desafíos y enigmas que desafían la mente de Pylonauta.',
+            date: '4 de abril de 2250',
         },
         {
             id: 'sixth',
             title: 'Sixth Photo',
             img: '/_eb17826d-286f-4c61-9250-a88dcdea07a8.jpeg',
-            date: '5 de abril de 2250',
-            gif: '/parte6.gif',
-            text: 'Sin embargo, con determinación y coraje, él se enfrenta a cada obstáculo con valentía. Está decidido a reparar su nave y regresar a casa, sin importar cuán difíciles sean las pruebas que enfrenta.',
+            date: '4 de abril de 2250',
         },
     ]
 
-    const audioHistoriaEpica = '/historia_epica.mp3'
     const audioBienvenida = '/bienvenida.mp3'
 
     // Función para avanzar/retroceder la foto
@@ -95,19 +82,25 @@ export default function HistoriaEpica() {
             // Calcular el nuevo índice de la foto activa
             return nuevoIndice
         })
-
-        if (activePhoto == 0) {
-            playAudio(22, 36, audioHistoriaEpica)
-        } else if (activePhoto == 1) {
-            playAudio(36, 51, audioHistoriaEpica)
-        } else if (activePhoto == 2) {
-            playAudio(51, 67.5, audioHistoriaEpica)
-        } else if (activePhoto == 3) {
-            playAudio(67.5, 82, audioHistoriaEpica)
-        } else if (activePhoto == 4) {
-            playAudio(82, 99, audioHistoriaEpica)
-        }
     }
+
+    useEffect(() => {
+        if (!showOverlay) {
+            if (activePhoto == 1) {
+                playSegment(0, 19.6)
+            } else if (activePhoto == 2) {
+                playSegment(20, 33)
+            } else if (activePhoto == 3) {
+                playSegment(33.5, 49)
+            } else if (activePhoto == 4) {
+                playSegment(49, 66)
+            } else if (activePhoto == 5) {
+                playSegment(66.5, 82)
+            } else if (activePhoto == 6) {
+                playSegment(82.5, 96)
+            }
+        }
+    }, [activePhoto])
 
     const previousPhoto = () => {
         handleHoverChange('')
@@ -118,6 +111,23 @@ export default function HistoriaEpica() {
             // Calcular el nuevo índice de la foto activa
             return nuevoIndice
         })
+    }
+
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const [currentTime, setCurrentTime] = useState(0)
+
+    const playSegment = (startTime: number, endTime: number) => {
+        if (videoRef.current) {
+            videoRef.current.currentTime = startTime
+            videoRef.current.play()
+
+            const timeout = setTimeout(() => {
+                videoRef.current?.pause()
+                setCurrentTime(endTime)
+            }, (endTime - startTime) * 1000)
+
+            return () => clearTimeout(timeout)
+        }
     }
 
     // Función para manejar el cambio de clases de hover
@@ -147,66 +157,64 @@ export default function HistoriaEpica() {
 
         setTimeout(() => {
             setShowOverlay(false)
-            playAudio(0, 21.3, audioHistoriaEpica)
+            setActivePhoto(1)
         }, 3000)
     }
 
-    if (showOverlay) {
-        return (
-            <div className="overlay flex flex-col items-center justify-center">
-                <div className="flex gap-2 items-center justify-center mx-10">
-                    <Isotipo className="w-20 sm:w-56 text-white" />
-                    <Logo className="w-60 sm:w-80 md:w-full text-white" />
-                </div>
-                <Button
-                    onClick={init}
-                    className="mt-20 font-bold rounded-full text-3xl bg-primary hover:bg-primary/90 transition-colors text-white hover:text-white/50 p-6">
-                    Empezar
-                </Button>
-            </div>
-        )
-    }
-
     return (
-        <div className="grid">
-            <div className="lg:grid lg:grid-cols-2 place-items-center flex items-center justify-center h-screen overflow-hidden bg-[url('/fondo-introduccion.webp')] bg-cover bg-center [perspective:500px]">
-                <div className="peer/previous blob-left group relative bottom-[45vh] right-[6rem] lg:bottom-0 lg:right-0 lg:flex lg:h-full lg:w-full md:items-center md:pb-0 lg:mr-[22rem]">
-                    <ActionButton
-                        direction="left"
-                        text="Foto anterior"
-                        onClick={previousPhoto}
-                        disabled={activePhoto == 0 || (isPlaying && !showOverlay)}
+        <>
+            {showOverlay && (
+                <div className="overlay flex flex-col items-center justify-center">
+                    <div className="flex gap-2 items-center justify-center mx-10">
+                        <Isotipo className="w-20 sm:w-56 text-white" />
+                        <Logo className="w-60 sm:w-80 md:w-full text-white" />
+                    </div>
+                    <Button
+                        onClick={init}
+                        className="mt-20 font-bold rounded-full text-2xl !px-10 bg-primary hover:bg-primary/90 transition-colors text-white hover:text-white/50 p-6">
+                        Empezar
+                    </Button>
+                </div>
+            )}
+
+            <div className="grid">
+                <div className="lg:grid lg:grid-cols-2 place-items-center flex items-center justify-center h-screen overflow-hidden bg-[url('/fondo-introduccion.webp')] bg-cover bg-center [perspective:500px]">
+                    <div className="peer/previous blob-left group relative bottom-[45vh] right-[6rem] lg:bottom-0 lg:right-0 lg:flex lg:h-full lg:w-full md:items-center md:pb-0 lg:mr-[22rem]">
+                        <ActionButton
+                            direction="left"
+                            text="Foto anterior"
+                            onClick={previousPhoto}
+                            disabled={activePhoto == 1 || (isPlaying && !showOverlay)}
+                        />
+                    </div>
+                    <div className="peer/next blob-right group relative bottom-[45vh] -right-[6rem] lg:bottom-0 lg:right-0 lg:flex lg:h-full lg:w-full md:items-center md:pb-0 lg:ml-[22rem]">
+                        {activePhoto != 6 ? (
+                            <ActionButton
+                                direction="right"
+                                text="Siguiente foto"
+                                onClick={nextPhoto}
+                                disabled={isPlaying && !showOverlay}
+                            />
+                        ) : (
+                            <Button
+                                onClick={handleButton}
+                                className="button-underline relative flex w-full items-center justify-center font-bold transition-[transform,color] bg-transparent hover:bg-transparent duration-500 focus-visible:text-white group-hover:text-white lg:text-2xl xl:text-4xl">
+                                Continuar
+                            </Button>
+                        )}
+                    </div>
+
+                    <Photo
+                        img={photosData[activePhoto].img}
+                        date={photosData[activePhoto].date}
+                        className={twMerge('z-10', hoverClass)}
+                        title={photosData[activePhoto].title}
+                        showOverlay={showOverlay}
+                        videoRef={videoRef}
                     />
                 </div>
-                <div className="peer/next blob-right group relative bottom-[45vh] -right-[6rem] lg:bottom-0 lg:right-0 lg:flex lg:h-full lg:w-full md:items-center md:pb-0 lg:ml-[22rem]">
-                    {activePhoto != 5 ? (
-                        <ActionButton
-                            direction="right"
-                            text="Siguiente foto"
-                            onClick={nextPhoto}
-                            disabled={isPlaying && !showOverlay}
-                        />
-                    ) : (
-                        <Button
-                            onClick={handleButton}
-                            className="button-underline relative flex w-full items-center justify-center font-bold transition-[transform,color] bg-transparent hover:bg-transparent duration-500 focus-visible:text-white group-hover:text-white lg:text-2xl xl:text-4xl">
-                            Continuar
-                        </Button>
-                    )}
-                </div>
-
-                <Photo
-                    img={photosData[activePhoto].img}
-                    date={photosData[activePhoto].date}
-                    text={photosData[activePhoto].text}
-                    gif={photosData[activePhoto].gif}
-                    className={twMerge('z-10', hoverClass)}
-                    title={photosData[activePhoto].title}
-                    showOverlay={showOverlay}
-                    isPlaying={isPlaying}
-                />
             </div>
-        </div>
+        </>
     )
 }
 
@@ -215,21 +223,16 @@ const Photo = ({
     title,
     date,
     img,
-    text,
-    gif,
     zIndex,
-    showOverlay,
-    isPlaying,
+    videoRef,
 }: {
     className?: string
     title?: string
     date: string
     img: string
-    text?: string
-    gif?: string
     zIndex?: number
     showOverlay: boolean
-    isPlaying: boolean
+    videoRef: React.RefObject<HTMLVideoElement>
 }) => {
     return (
         <div
@@ -251,13 +254,12 @@ const Photo = ({
                 </picture>
 
                 <div className="overflow-hidden">
-                    {isPlaying && !showOverlay && (
-                        <img
-                            className="relative top-[-40px] md:top-[-60px] xl:top-[-74px] 2xl:top-[-80px] scale-[1.8]"
-                            src={gif}
-                            alt=""
+                    <video ref={videoRef}>
+                        <source
+                            src="/grabacion.mp4"
+                            type="video/mp4"
                         />
-                    )}
+                    </video>
                 </div>
             </div>
             <div className="pointer-events-none rounded-3xl bg-white [grid-area:1/1] [backface-visibility:hidden] [transform:rotateY(180deg)]" />
