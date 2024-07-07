e0973dbbe9b781dcfb1933f5a68fd62abd0d20b0
carincon93 <carincon93@gmail.com>
2024-07-07 10:41:12 -0500

    OrbitControls to debug added

--- app/components/game/anfora/AnforaExperience.jsx
+++ app/components/game/anfora/AnforaExperience.jsx
@@ -3,32 +3,46 @@ import { CharacterController } from '../CharacterController'
 import { useRef } from 'react'
 import { Environment, OrbitControls, OrthographicCamera } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+
+const CameraComponent = () => {
+    const controlsRef = useRef()
+    const cameraRef = useRef()
+
+    useFrame(() => {
+        if (controlsRef.current) {
+            const { x, y, z } = controlsRef.current.object.position
+            console.log(`Camera position: x: ${x}, y: ${y}, z: ${z}`)
+        }
+    })
+
+    return (
+        <>
+            <perspectiveCamera
+                ref={cameraRef}
+                position={[0, 0, 5]}
+            />
+            <OrbitControls ref={controlsRef} />
+        </>
+    )
+}
 
 export const AnforaExperience = () => {
     const shadowCameraRef = useRef()
 
     return (
         <>
-            {/* <OrbitControls /> */}
+            {/* <CameraComponent /> */}
             <Environment preset="sunset" />
             <directionalLight
                 intensity={0.65}
                 castShadow
-                position={[-15, 10, 15]}
+                position={[-15, 20, 15]}
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
-                shadow-bias={-0.00005}>
-                <OrthographicCamera
-                    left={-22}
-                    right={15}
-                    top={10}
-                    bottom={-20}
-                    ref={shadowCameraRef}
-                    attach={'shadow-camera'}
-                />
-            </directionalLight>
+                shadow-bias={-0.00005}></directionalLight>
             <Physics debug={process.env.NEXT_PUBLIC_DEBUG == 'true'}>
-                <Map position={[-6, -7, 0]} />
+                <Map position={[-6, -6.56, 0]} />
                 <CharacterController />
             </Physics>
         </>
