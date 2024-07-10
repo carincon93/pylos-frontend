88fc72458b89d27cac793a1e12a7cb1abc2d0d7c
carincon93 <carincon93@gmail.com>
2024-07-10 09:13:23 -0500

    mundos link added

--- app/juego/layout.tsx
+++ app/juego/layout.tsx
@@ -1,6 +1,6 @@
 import { getProfile } from '@/lib/actions'
 import Link from 'next/link'
-import { RESULTADOS_ROUTE } from '@/utils/routes'
+import { MUNDOS_ROUTE, RESULTADOS_ROUTE } from '@/utils/routes'
 
 export default async function AuthLayout({
     children, // will be a page or nested layout
@@ -12,24 +12,44 @@ export default async function AuthLayout({
     return (
         <div>
             {profile?.esAdmin && (
-                <Link
-                    href={RESULTADOS_ROUTE + '?admin=1'}
-                    className="fixed bottom-4 right-4 z-30 bg-secondary hover:bg-secondary/90 rounded-full py-4 px-6 shadow-lg flex items-center justify-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="size-6 mr-2">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                        />
-                    </svg>
-                    Usuarios
-                </Link>
+                <>
+                    <Link
+                        href={RESULTADOS_ROUTE + '?admin=1'}
+                        className="fixed bottom-4 right-4 z-30 bg-secondary hover:bg-secondary/90 rounded-full py-4 px-6 shadow-lg flex items-center justify-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6 mr-2">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                            />
+                        </svg>
+                        Usuarios
+                    </Link>
+                    <Link
+                        href={MUNDOS_ROUTE}
+                        className="fixed bottom-20 right-4 z-30 bg-secondary hover:bg-secondary/90 rounded-full py-4 px-6 shadow-lg flex items-center justify-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6 mr-2">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
+                            />
+                        </svg>
+                        Mundos
+                    </Link>
+                </>
             )}
             {children}
         </div>
