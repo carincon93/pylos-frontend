e6efb84822962942fd47a55004e0cb2cf9450832
carincon93 <carincon93@gmail.com>
2024-07-17 09:55:17 -0500

    loading modified

--- app/loading.tsx
+++ app/loading.tsx
@@ -19,21 +19,24 @@ export default function LoadingOverlay({ className }: Props) {
 
     return (
         <>
-            <div className={`${className ? className : ''} bg-pylos-900 fixed left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex items-center justify-center text-4xl font-bold`}>
-                <div className="mr-2">
-                    PYLOS <span className="font-light">|</span>{' '}
+            <div
+                className={`${
+                    className ? className : ''
+                } bg-pylos-900 fixed left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex flex-col sm:flex-row items-center justify-center text-4xl font-bold`}>
+                <div className="mr-0 sm:mr-2 ml-4 sm:ml-0">
+                    PYLOS <span className="font-light invisible sm:visible relative -top-1">|</span>{' '}
                 </div>
-                <div className="word-wrapper font-edu">
+                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
                     <span className={`word1 ${toggle ? 'anim1Word1' : 'anim2Word1'} transition-transform delay-75`}>Diversión, </span>
                 </div>
-                <div className="word-wrapper font-edu">
+                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
                     <span className={`word2 ${toggle ? 'anim1Word2' : 'anim2Word2'} ml-2 transition-transform delay-75`}>Aventura, </span>
                 </div>
-                <div className="word-wrapper font-edu">
+                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
                     <span className={`word3 ${toggle ? 'anim1Word3' : 'anim2Word3'} ml-2 transition-transform delay-75`}>Aprendizaje.</span>
                 </div>
             </div>
-            <div className="loader"></div>
+            <div className="loader !top-[350px] sm:!top-[250px]"></div>
         </>
     )
 }
