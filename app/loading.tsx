4d69e45b4da7b65dfdd7d0d66ef93c7cd4d7a3c1
carincon93 <carincon93@gmail.com>
2024-07-17 09:46:25 -0500

    loading modified

--- app/loading.tsx
+++ app/loading.tsx
@@ -18,16 +18,22 @@ export default function LoadingOverlay({ className }: Props) {
     }, [])
 
     return (
-        <div className={`${className ? className : ''} bg-pylos-900 fixed left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex items-center justify-center text-4xl font-bold font-edu`}>
-            <div className="word-wrapper">
-                <span className={`word1 ${toggle ? 'anim1Word1' : 'anim2Word1'} transition-transform delay-75`}>Diversión, </span>
+        <>
+            <div className={`${className ? className : ''} bg-pylos-900 fixed left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex items-center justify-center text-4xl font-bold`}>
+                <div className="mr-2">
+                    PYLOS <span className="font-light">|</span>{' '}
+                </div>
+                <div className="word-wrapper font-edu">
+                    <span className={`word1 ${toggle ? 'anim1Word1' : 'anim2Word1'} transition-transform delay-75`}>Diversión, </span>
+                </div>
+                <div className="word-wrapper font-edu">
+                    <span className={`word2 ${toggle ? 'anim1Word2' : 'anim2Word2'} ml-2 transition-transform delay-75`}>Aventura, </span>
+                </div>
+                <div className="word-wrapper font-edu">
+                    <span className={`word3 ${toggle ? 'anim1Word3' : 'anim2Word3'} ml-2 transition-transform delay-75`}>Aprendizaje.</span>
+                </div>
             </div>
-            <div className="word-wrapper">
-                <span className={`word2 ${toggle ? 'anim1Word2' : 'anim2Word2'} ml-2 transition-transform delay-75`}>Aventura, </span>
-            </div>
-            <div className="word-wrapper">
-                <span className={`word3 ${toggle ? 'anim1Word3' : 'anim2Word3'} ml-2 transition-transform delay-75`}>Aprendizaje.</span>
-            </div>
-        </div>
+            <div className="loader"></div>
+        </>
     )
 }
