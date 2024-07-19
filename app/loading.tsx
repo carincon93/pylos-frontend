5b28c26ac06b20a82de4174099f069187130c66f
carincon93 <carincon93@gmail.com>
2024-07-19 11:24:35 -0500

    loading updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -8,8 +8,12 @@ interface Props {
 
 export default function LoadingOverlay({ className }: Props) {
     const [toggle, setToggle] = useState(true)
+    const [showLoading, setShowLoading] = useState(false)
 
     useEffect(() => {
+        setTimeout(() => {
+            setShowLoading(true)
+        }, 1500)
         const interval = setInterval(() => {
             setToggle((prevToggle) => !prevToggle)
         }, 3500)
@@ -18,25 +22,31 @@ export default function LoadingOverlay({ className }: Props) {
     }, [])
 
     return (
-        <>
-            <div
-                className={`${
-                    className ? className : ''
-                } bg-pylos-900 fixed left-0 top-0 right-0 w-full h-[100vh] z-[9999] text-white flex flex-col sm:flex-row items-center justify-center text-4xl font-bold`}>
-                <div className="mr-0 sm:mr-2 ml-4 sm:ml-0">
-                    PYLOS <span className="font-light invisible sm:visible relative -top-1">|</span>{' '}
+        <div className="bg-pylos-900 fixed inset-0 w-full h-[100vh] z-10">
+            {showLoading && (
+                <div className={`absolute z-[10000] left-0 right-0 mx-auto text-center transition-opacity delay-75 duration-500 bottom-10 font-bold text-3xl ${toggle ? 'opacity-100' : 'opacity-20'}`}>
+                    PYLOS
                 </div>
-                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
-                    <span className={`word1 ${toggle ? 'anim1Word1' : 'anim2Word1'} transition-transform delay-75`}>Diversión, </span>
-                </div>
-                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
-                    <span className={`word2 ${toggle ? 'anim1Word2' : 'anim2Word2'} ml-2 transition-transform delay-75`}>Aventura, </span>
-                </div>
-                <div className="word-wrapper font-edu relative top-6 sm:top-0 text-[26px]">
-                    <span className={`word3 ${toggle ? 'anim1Word3' : 'anim2Word3'} ml-2 transition-transform delay-75`}>Aprendizaje.</span>
-                </div>
+            )}
+            <div className={`${className ? className : ''} fixed inset-0 w-full h-[100vh] z-[10000] bg-transparent text-white flex flex-row items-center justify-center font-bold`}>
+                {showLoading && (
+                    <>
+                        <div className="word-wrapper font-edu top-6 sm:top-0 text-[26px] relative">
+                            <span className={`word1 ${toggle ? 'anim1Word1' : 'anim2Word1'} transition-transform delay-75`}>Diversión, </span>
+                        </div>
+                        <div className="word-wrapper font-edu top-6 sm:top-0 text-[26px] relative">
+                            <span className={`word2 ${toggle ? 'anim1Word2' : 'anim2Word2'} ml-2 transition-transform delay-75`}>Aventura, </span>
+                        </div>
+                        <div className="word-wrapper font-edu top-6 sm:top-0 text-[26px] relative">
+                            <span className={`word3 ${toggle ? 'anim1Word3' : 'anim2Word3'} ml-2 transition-transform delay-75`}>Aprendizaje.</span>
+                        </div>
+                    </>
+                )}
             </div>
-            <div className="loader !top-[350px] sm:!top-[250px]"></div>
-        </>
+            <div className={`transition-transform absolute z-[10000] inset-0 size-[35px] m-auto delay-75 duration-500 ${showLoading ? 'translate-y-40' : 'translate-y-0'} `}>
+                <span className="loader"></span>
+            </div>
+            <div className={`bg-pylos-800/60 fixed z-[9999] h-[200%] inset-0 transform transition-transform rotate-[45deg]`}></div>
+        </div>
     )
 }
