a18ddd934469f8e02285ea58175ec9f411ae5012
carincon93 <carincon93@gmail.com>
2024-08-05 10:18:01 -0500

    instructions updated

--- app/juego/anfora/page.tsx
+++ app/juego/anfora/page.tsx
@@ -516,6 +516,26 @@ function Anfora() {
 
                                         <small className="block text-center ml-10">| Restablecer posición</small>
                                     </div>
+
+                                    <div className="flex items-center !mt-10">
+                                        <div className="w-14 mx-16 p-2">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="1.5"
+                                                stroke="currentColor"
+                                                className="size-10 sm:size-12 text-black">
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                                                />
+                                            </svg>
+                                        </div>
+
+                                        <small className="block text-center ml-10">| Botón para restablecer posición</small>
+                                    </div>
                                 </AlertDialogHeader>
                                 <AlertDialogFooter className="mt-10">
                                     <AlertDialogCancel
